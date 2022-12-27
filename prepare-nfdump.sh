95bc0aabf6921af99d117c84df376dbe5e32f5f9
TP <tpoder@netx.as>
2022-12-27 16:57:23 +0100

    new version 1.31, updated to nfdump 1.6.25

--- prepare-nfdump.sh
+++ prepare-nfdump.sh
@@ -21,9 +21,9 @@
 #
 
 
-NFDUMP_VERSION="1.6.22"
+NFDUMP_VERSION="1.6.25"
 NFDUMP="nfdump-$NFDUMP_VERSION"
-NFDUMP_MD5="25aa5f12a781c984e8538c4f54ca32ce"
+NFDUMP_MD5="9c79e52b5b00c7ae7a31e8c681cf17cb"
 NFDUMP_SRC="$NFDUMP.tar.gz"
 NFDUMP_URL="https://github.com/phaag/nfdump/archive/v$NFDUMP_VERSION.tar.gz"
 
